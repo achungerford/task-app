bbe253f5344be401b365ba783e8a7f264974fed9
Alex Hungerford <10031447+achungerford@users.noreply.github.com>
2020-04-14 16:49:31 -0700

    removed CREATE functions insertOne and insertMany

--- mongodb.js
+++ mongodb.js
@@ -17,55 +17,5 @@ MongoClient.connect(connectionURL, { useUnifiedTopology: true, useNewUrlParser: 
 
     // creating a connection for a specific database
     const db = client.db(databaseName);
-
-    // insert a single document into a collection called 'users'
-    db.collection('users').insertOne({
-        name: 'Obama',
-        age: 58
-    }, (error, result) => {
-        if (error) {
-            return console.log('Unable to insert user.');
-        }
-
-        console.log(result.ops);
-    })
-
-    
-//    // how to bulk-insert documents into a collection
-    // db.collection('users').insertMany([
-    //     {
-    //         name: 'Jen',
-    //         age: 28
-    //     }, {
-    //         name: 'Alex',
-    //         age: 30
-    //     }
-    // ], (error, result) => {
-    //     if (error) {
-    //         return console.log('Unable to insert documents');
-    //     }
-
-    //     console.log(result.ops);
-    // })
-
-
-    // db.collection('tasks').insertMany([
-    //     {
-    //         description: 'Eat breakfast',
-    //         completed: true
-    //     }, {
-    //         description: 'Make lunch',
-    //         completed: true
-    //     }, {
-    //         description: 'Cook dinner',
-    //         completed: false
-    //     }
-    // ], (error, result) => {
-    //     if (error) {
-    //         return console.log('Unable to insert tasks!');
-    //     }
-
-    //     console.log(result.ops);
-    // })
 })
 
