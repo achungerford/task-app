66a31d6f4f8afeb941d47a6cf42faaf5f9c1b4b9
Alex Hungerford <10031447+achungerford@users.noreply.github.com>
2020-04-20 19:29:51 -0700

    set up mongoose connection to DB

---
+++ src/db/mongoose.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+// connecting to database & including options object
+// mongodb protocol, local IP address, port: 27017, database name: task-app-api
+mongoose.connect('mongodb://127.0.0.1:27017/task-app-api', {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex: true
+})
