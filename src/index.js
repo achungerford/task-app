b01c9214120c269437a790bcc7f4f26f47116d1c
Alex Hungerford <10031447+achungerford@users.noreply.github.com>
2020-04-23 15:23:38 -0700

    config users route

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
+// configure users route/endpoint
+app.post('/users', (req, res) => {
+    res.send('testing');
+})
+
 app.listen(port, () => {
     console.log(`Server is up on ${port}`);
 })
