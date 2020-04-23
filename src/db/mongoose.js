87b1682ffa7395d5ab4419343886ace434cf2d0d
Alex Hungerford <10031447+achungerford@users.noreply.github.com>
2020-04-23 14:59:58 -0700

    del mongodb file, install express and nodedmon

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -49,39 +49,42 @@ const User = mongoose.model('User', {
     }
 })
 
-// create an instance of the model
-const me = new User({
-    name: 'John      ',
-    email: '   MYEMAIL@MEAD.IO',
-    password: 'phone123'
-})
-
-// use methods to save the model instance into DB
-me.save().then(() => {
-    console.log(me);
-}).catch((error) => {
-    console.log('Error!', error);
-})
-
-// // create Task model
-// const Task = mongoose.model('Task', {
-//     description: {
-//         type: String
-//     },
-//     completed: {
-//         type: Boolean
-//     }
+// // create an instance of the model
+// const me = new User({
+//     name: 'John      ',
+//     email: '   MYEMAIL@MEAD.IO',
+//     password: 'phone123'
 // })
 
-// // create instance of Task
-// const task = new Task({
-//     description: 'Learn Mongoose libary.',
-//     completed: false
-// })
-
-// // use 'save()' method to save the instance into DB
-// task.save().then(() => {
-//     console.log(task);
+// // use methods to save the model instance into DB
+// me.save().then(() => {
+//     console.log(me);
 // }).catch((error) => {
-//     console.log(error);
+//     console.log('Error!', error);
 // })
+
+// create Task model
+const Task = mongoose.model('Task', {
+    description: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    completed: {
+        type: Boolean,
+        required: false,
+        default: false
+    }
+})
+
+// create instance of Task
+const task = new Task({
+    description: 'Learn Mongoose libary.'
+})
+
+// use 'save()' method to save the instance into DB
+task.save().then(() => {
+    console.log(task);
+}).catch((error) => {
+    console.log(error);
+})
