fce25f12f0286a632a0ff75a06c63ed898768117
Alex Hungerford <10031447+achungerford@users.noreply.github.com>
2020-04-24 10:21:55 -0700

    refactor

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -9,45 +9,7 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-app-api', {
     useCreateIndex: true
 })
 
-// define our first model - basic version of 'user' model
-const User = mongoose.model('User', {
-    name: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    email: {
-        type: String,
-        required: true,
-        trim: true,
-        lowercase: true,
-        validate(value) {
-            if (!validator.isEmail(value)) {
-                throw new Error('Email is invalid.');
-            }
-        }
-    },
-    password: {
-        type: String,
-        required: true,
-        minlength: 7,
-        trim: true,
-        validate(value) {
-            if (value.toLowerCase().includes('password')) {
-                throw new Error('Password cannon contain "password"');
-            }
-        }
-    },
-    age: {
-        type: Number,
-        default: 0,
-        validate(value) {
-            if (value < 0) {
-                throw new Error('Age must be a positive number');
-            }
-        }
-    }
-})
+
 
 // // create an instance of the model
 // const me = new User({
