bb3913421e89bab80af93c7618ba61503773843e
Alex Hungerford <10031447+achungerford@users.noreply.github.com>
2020-04-28 13:43:54 -0700

    removed newUrlParser bc deprecated

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -4,6 +4,5 @@ const mongoose = require('mongoose');
 // mongodb protocol, local IP address, port: 27017, database name: task-app-api
 mongoose.connect('mongodb://127.0.0.1:27017/task-app-api', {
     useUnifiedTopology: true,
-    useNewUrlParser: true,
     useCreateIndex: true
 })
