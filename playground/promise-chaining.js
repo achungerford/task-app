96ffd43456f18b46963a60285f4cd452263990fc
Alex Hungerford <10031447+achungerford@users.noreply.github.com>
2020-05-05 13:39:03 -0700

    promise chaining example

--- playground/promise-chaining.js
+++ playground/promise-chaining.js
@@ -1,4 +1,11 @@
  require('../src/db/mongoose');
  const User = require('../src/models/user');
 
- 
+User.findByIdAndUpdate('5eab98bc08227338685a0294', { age: 1 }).then((user)=>{
+    console.log(user);
+    return User.countDocuments({ age: 1 });
+}).then((result) => {
+    console.log(result);
+}).catch((e) => {
+    console.log(e);
+})
