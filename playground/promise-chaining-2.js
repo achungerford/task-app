4ceb9753c61437e9f7610e356f3830f4dd2d9bc4
Alex Hungerford <10031447+achungerford@users.noreply.github.com>
2020-05-06 12:03:03 -0700

    a fn returns undefined if nothing explicitly returned

--- playground/promise-chaining-2.js
+++ playground/promise-chaining-2.js
@@ -2,7 +2,7 @@ require('../src/db/mongoose');
 const Task = require('../src/models/task');
 
 // remove a given task by id
-// get and print the 
+// get and print the total number of incomplete tasks
 
 Task.findByIdAndDelete('5ea1c0a64a7a6926a8a4ed53').then(() => {
     return Task.countDocuments({ completed: false });
