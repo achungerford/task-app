bf78b13bc3da0982ba14751bb39cacdea30d8947
Alex Hungerford <10031447+achungerford@users.noreply.github.com>
2020-05-06 17:57:40 -0700

    demo async-await; use comments

--- playground/async-await.js
+++ playground/async-await.js
@@ -1,13 +1,29 @@
 const add = (a, b) => {
-    return new Promise((resolve, reject) => {
+    return new Promise((resolve, reject) => {   // add() returns a Promise
         setTimeout(() => {
-            resolve(a + b);
-        }, 2000)
+            if (a < 0 || b < 0) {       // a, b must be positive
+                return reject('Numbers must be positive.')
+            }
+
+            resolve(a + b);     // runs if a & b are positive
+
+        }, 2000)    // 2 second delay
     })
 }
 
+// defining the doWork function with async-await
 const doWork = async () => {
-    return 'Jordan';
+    const sum = await add(1, 99)        // wait 2 seconds, 100
+    const sum2 = await add(sum, 50)     // wait 2 seconds, 150
+    const sum3 = await add(sum2, 3)     // wait 2 seconds, 153
+    return sum3;    // none of the numbers above appear; after 6 seconds we get 153
 }
+
+// calling doWork using async-await instead of promise-chaining with multiple then-stmts
+doWork().then((result) => {
+    console.log('result', result);
+}).catch((e) => {
+    console.log('e', e);
+})
 
 console.log(doWork())
